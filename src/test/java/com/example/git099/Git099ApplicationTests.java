498cd5c78687d0caa3d831c62438e497c0835760
1612835228 <1612835228@qq.com>
2020-09-09 17:57:16 +0800

    创建一个项目

---
+++ src/test/java/com/example/git099/Git099ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git099;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Git099ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
